110172f542c1ab2720ebcb401f5a9ded6d8d92bb
MarqCervMartin <martin_mcervantes@hotmail.com>
2020-09-28 03:08:12 -0500

    feat: add the last features, print the frecuency of class and sort of the distance of K point with index.

--- KNN/src/knn/CoordenadasClases.java
+++ KNN/src/knn/CoordenadasClases.java
@@ -10,12 +10,15 @@ package knn;
  * @author martin
  */
 public class CoordenadasClases {
-    private int x,y;
+    private int x,y,c;
+    private float distance;
     
     //COnstructor
-    public CoordenadasClases(int x, int y){
+    public CoordenadasClases(int x, int y, int c, float d){
         this.x = x;
         this.y = y;
+        this.c = c;
+        this.distance = d;
     }
     //Setters and getters
     public int getX() {
@@ -34,8 +37,28 @@ public class CoordenadasClases {
         this.y = y;
     }
 
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
     @Override
     public String toString() {
-        return "KNN{" + "x=" + x + ", y=" + y + '}';
+        return "CoordenadasClases{" + "x=" + x + ", y=" + y + ", c=" + c + ", distance=" + distance + '}';
     }
+
+    
+    
+    
 }
