7011557e0cc49712b758ee3825f2e6b7f4c2ef5a
EstebanSADCV <estebansonic_1998@hotmail.com>
2020-11-29 21:59:22 -0600

    subi el AG Horarios... by STEWARD

---
+++ Proyecto/AG_Horarios/src/ag_horarios/AG_Horarios.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ag_horarios;
+
+/**
+ *
+ * @author liraz
+ */
+public class AG_Horarios {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Funciones F = new Funciones();
+        F.generar_padre(7);
+    }
+}
